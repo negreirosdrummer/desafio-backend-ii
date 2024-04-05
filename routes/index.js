d42447e91345f2fe1c1625fd90636685da26abba
Rafael Negreiros <142756979+negreirosdrummer@users.noreply.github.com>
2024-04-05 14:52:24 -0300

    Integrando módulos nodemon e mysql2

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
-
+const mysql = require('mysql2/promise');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {title: 'Express'});
+router.get('/', function (req, res, next) {
+  mysql.createConnection({
+    host: 'localhost', user: 'root', password: '',
+    database: 'desafio-backend-ii', port: 3306
+  }).then((connection) => {
+    connection.query('SELECT * FROM clientes;')
+    .then((result) => { res.send(result[0]); });
+  });
 });
-
 module.exports = router;
