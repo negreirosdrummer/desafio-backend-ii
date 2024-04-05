4e7b5d629f447f212c52b6bf3b4c2f74293b2bd6
Rafael Negreiros <142756979+negreirosdrummer@users.noreply.github.com>
2024-04-05 09:58:07 -0300

    ESLint OK

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-//Chame o módulo HTTP
-var http = require("http");
-//Crie um servidor HTTP para ouvir as requisições na porta 8000
-http.createServer(function (req, resp) {
-  // Configure o resposta HTTP header com o HTTP status e Content type
-  resp.writeHead(200, { 'Content-Type': 'text/plain' });
-  // Envie a resposta do body "Ola Mundo!"
-  resp.end('Ola Mundo!\n');
-}).listen(8000);
+const express = require('express');
+const app = express();
+
+// Crie um endpoint para receber requisições
+app.get('/', function(req, res) {
+  res.send('Ola Mundo!');
+});
+
 // Imprima URL para acessar o servidor
-console.log('Server running at http://localhost:8000/')
+app.listen(8000, function() {
+  console.log('App ouvindo na porta 8000!');
+});
