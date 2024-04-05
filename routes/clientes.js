a0a9d7c309dae8fd6a564c7049477b66b5b92e99
Rafael Negreiros <142756979+negreirosdrummer@users.noreply.github.com>
2024-04-05 17:33:06 -0300

    Criando configs, controllers, middlewares, services

---
+++ routes/clientes.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const clienteController = require('../controllers/clienteController');
+const nomeMiddleware = require('../middlewares/nomeMiddleware');
+const sobrenomeMiddleware = require('../middlewares/sobrenomeMiddleware');
+const idadeMiddleware = require('../middlewares/idadeMiddleware');
+/* GET clientes*/
+router.get('/', clienteController.findAll);
+/* POST clientes*/
+router.post('/', nomeMiddleware.validateName,
+  sobrenomeMiddleware.validateFamilyName,
+  idadeMiddleware.validateAge,
+  clienteController.save
+);/* PUT clientes*/
+router.put('/', clienteController.update);
+/* DELETE clientes*/
+router.delete('/:id', clienteController.remove);
+module.exports = router;
