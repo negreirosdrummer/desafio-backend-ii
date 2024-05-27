ac2a84eff4d3b7192c8cdf647e256d1647580b3b
Rafael Negreiros <142756979+negreirosdrummer@users.noreply.github.com>
2024-05-27 14:29:24 -0300

    inciando implementação de caching

--- controllers/clienteController.js
+++ controllers/clienteController.js
@@ -1,4 +1,5 @@
 const clienteService = require('../services/clienteService');
+
 const findAll = async (request, response) => {
   const clientes = await clienteService.findAll();
   return response.status(200).json(clientes);
@@ -21,7 +22,7 @@ const update = async (request, response) => {
 };
 
 const remove = async (request, response) => {
-  const {id} = request.params;
+  const { id } = request.params;
   const result = await clienteService.remove(id);
   return result ?
     response.status(200).json() : response.status(400).json({
--- services/clienteService.js
+++ services/clienteService.js
@@ -1,14 +1,26 @@
 const connection = require('../configs/dbConfiguration');
+const cache = require('../configs/cache');
+
 const findAll = async () => {
-  const clientes = await (await connection)
-      .execute('SELECT * FROM clientes');
-  return clientes[0];
+  const cacheKey = 'all_clients';
+  if (cache.has(cacheKey)) {
+    console.log('Requisição feita ao cache');
+    return cache.get(cacheKey);
+  } else {
+    console.log('Requisição feita ao banco de dados');
+    const clientes = await (await connection).execute('SELECT * FROM clientes');
+    cache.set(cacheKey, clientes[0]);
+    return clientes[0];
+  }
 };
 
 const update = async (cliente) => {
   const query = 'UPDATE clientes SET nome = ?, sobrenome = ?, email = ?, idade = ? WHERE id = ? ';
   const isOk = await (await connection).execute(query,
       [cliente.nome, cliente.sobrenome, cliente.email, cliente.idade, cliente.id]);
+  if (isOk[0].affectedRows === 1) {
+    cache.flushAll();
+  }
   return isOk[0].affectedRows === 1;
 };
 
@@ -16,12 +28,18 @@ const save = async (cliente) => {
   const query = 'INSERT INTO clientes(nome, sobrenome, email, idade) VALUES (?, ?, ?, ?)';
   const isOk = await (await connection).execute(query,
       [cliente.nome, cliente.sobrenome, cliente.email, cliente.idade]);
+  if (isOk[0].affectedRows === 1) {
+    cache.flushAll();
+  }
   return isOk[0].affectedRows === 1;
 };
 
 const remove = async (id) => {
   const query = 'DELETE FROM clientes WHERE id = ?';
   const isOk = await (await connection).execute(query, [id]);
+  if (isOk[0].affectedRows === 1) {
+    cache.flushAll();
+  }
   return isOk[0].affectedRows === 1;
 };
 
